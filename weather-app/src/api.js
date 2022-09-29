3523bbebdfcfdadfc74c46b257029ed6b5231820
Stolarek <przemyslaw.stolarek1@gmail.com>
2022-09-29 12:26:46 +0200

    Add search component, current-weather component

---
+++ weather-app/src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'c918d20cbcmshace74ad1b35739cp135e89jsn8cc59f3da3bf',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
---
+++ weather-app/src/components/search/search.js
@@ -0,0 +1,47 @@
+import { AsyncPaginate } from 'react-select-async-paginate';
+import { useState } from 'react';
+import { geoApiOptions, GEO_API_URL } from '../../api';
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = inputValue => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then(response => response.json())
+      .then(response => {
+        return {
+          options: response.data.map(city => ({
+            value: `${city.latitude} ${city.longitutde}`,
+            label: `${city.name} , ${city.countryCode}`,
+          })),
+        };
+      })
+      .catch(err => console.error(err));
+  };
+
+  const handleOnChange = searchData => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder='Search for city'
+      debounceTimeout={800}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
+
+export default Search;
+
+// {
+//         options: response.data.map(city=> {
+//          value: `${city.latitude} ${city.longitutde}`,
+//          label: `${city.name} , ${city.countryCode}`})
+//       }
